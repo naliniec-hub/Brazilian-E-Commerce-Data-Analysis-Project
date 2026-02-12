22a81f0005942a22608d353730d5074dba97ac70
naliniec-hub <naliniec@gmail.com>
2026-02-12 11:14:57 +0300

    Delete top_10_customers.sql

--- top_10_customers.sql
+++
@@ -1,8 +0,0 @@
-SELECT 
- customer_id,
- COUNT(DISTINCT(order_id)) AS total_orders,
- ROUND(SUM(order_revenue), 2) AS lifetime_value
-FROM `modern-photon-482107-t4.Brazil_Olist_Data.customer_sales`
-GROUP BY customer_id
-ORDER BY lifetime_value DESC
-LIMIT 10
