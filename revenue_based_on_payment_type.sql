a7740d6a831605f679fff948ed7c28aace95cd1f
naliniec-hub <naliniec@gmail.com>
2026-02-12 11:14:36 +0300

    Delete revenue_based_on_payment_type.sql

--- revenue_based_on_payment_type.sql
+++
@@ -1,7 +0,0 @@
-SELECT
-  payment_type,
-  ROUND(SUM(payment_value), 2) AS total_revenue,
-  COUNT(DISTINCT order_id) AS total_orders
-FROM `modern-photon-482107-t4.Brazil_Olist_Data.clean_Payment`
-GROUP BY payment_type
-ORDER BY total_revenue DESC
