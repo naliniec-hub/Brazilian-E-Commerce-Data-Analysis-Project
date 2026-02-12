0b02af8a66446b9849c86695e611b52b25e70fdb
naliniec-hub <naliniec@gmail.com>
2026-02-12 11:13:01 +0300

    Delete avg_delivery_day.sql

--- avg_delivery_day.sql
+++
@@ -1,5 +0,0 @@
-SELECT 
- ROUND(AVG(DATE_DIFF(order_delivered_customer_date, order_approved_at, DAY)), 2) AS avg_delivery_days
-FROM `modern-photon-482107-t4.Brazil_Olist_Data.clean_order_delivery`
-WHERE order_delivered_customer_date IS NOT NULL
- AND order_approved_at IS NOT NULL
