cc1cd805959bc6473ce5b1de5b098d1e58fe19c0
naliniec-hub <naliniec@gmail.com>
2026-02-12 11:13:53 +0300

    Delete life_time_value_customers.sql

--- life_time_value_customers.sql
+++
@@ -1,7 +0,0 @@
-SELECT 
-   customer_unique_id,
-   COUNT(DISTINCT(order_id)) AS total_orders,
-   SUM(order_revenue) AS ltv_customers
-FROM `modern-photon-482107-t4.Brazil_Olist_Data.unique_customers`
-GROUP BY customer_unique_id
-ORDER BY ltv_customers DESC;
