fb5573c2f39166ac46a045a4b49660609ff14f03
naliniec-hub <naliniec@gmail.com>
2026-02-12 11:14:16 +0300

    Delete payment_method_usage.sql

--- payment_method_usage.sql
+++
@@ -1,7 +0,0 @@
-SELECT 
-  payment_type,
-  COUNT(*) AS total_transaction,
-  ROUND(COUNT(*)*100/SUM(COUNT(*)) OVER(),2) AS usage_percentage
-FROM`modern-photon-482107-t4.Brazil_Olist_Data.clean_Payment`
-GROUP BY payment_type
-ORDER BY usage_percentage DESC
