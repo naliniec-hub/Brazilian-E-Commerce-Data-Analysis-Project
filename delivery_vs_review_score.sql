26b35b0ead9884a11f889ba2ac07b8696a4525e1
naliniec-hub <naliniec@gmail.com>
2026-02-12 11:13:32 +0300

    Delete delivery_vs_review_score.sql

--- delivery_vs_review_score.sql
+++
@@ -1,9 +0,0 @@
-SELECT 
- CASE 
- WHEN o.order_delivered_customer_date > o.order_estimated_delivery_date THEN "Late" ELSE "On Time" 
- END AS delivery_status,
- ROUND(AVG (s.review_score), 2) AS avg_review_score
-FROM `modern-photon-482107-t4.Brazil_Olist_Data.clean_order_delivery` o   
- JOIN `modern-photon-482107-t4.Brazil_Olist_Data.clean_seller_review_score` s 
- ON o.order_id = s.order_id
-GROUP BY delivery_status
