58c849c8ceb5f57c87663dfa0e939d3a009c1291
naliniec-hub <naliniec@gmail.com>
2026-02-12 11:14:05 +0300

    Delete monthly_revenue_trend.sql

--- monthly_revenue_trend.sql
+++
@@ -1,9 +0,0 @@
-SELECT 
- FORMAT_DATE('%Y-%m', DATE(o.order_approved_at)) AS year_month,
- ROUND (SUM(p.payment_value),2) AS total_revenue,
- COUNT (DISTINCT o.order_id) AS total_orders
-FROM `modern-photon-482107-t4.Brazil_Olist_Data.clean_orders_final` o
-JOIN `modern-photon-482107-t4.Brazil_Olist_Data.clean_payment_final` p
-ON o.order_id = p.order_id
-GROUP BY year_month
-ORDER BY year_month;
